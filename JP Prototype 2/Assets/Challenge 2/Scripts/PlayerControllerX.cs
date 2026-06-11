f45ffa75f56b362007d8ea14a2bf50a073c1dae2
Finn191 <thanhphungab@gmail.com>
2026-06-11 11:20:45 +0700

    complete challenge 2

---
+++ JP Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnDelay;
+    private float spawnCountDown;
+
+    void Update()
+    {
+        spawnCountDown -= Time.deltaTime;
+        if (spawnCountDown <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnCountDown = spawnDelay;
+        }
+    }
+}
---
+++ JP Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnIntervalMin = 1.0f;
+    private float spawnIntervalMax = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnBallCoroutine());
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        Debug.Log("Spawn Ball");
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+    private IEnumerator SpawnBallCoroutine()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        while (true)
+        {
+            SpawnRandomBall();
+            float spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+}
