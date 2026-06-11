0afc4f1ec30bc48008ff0ed804f9ae08aaeff550
Finn191 <thanhphungab@gmail.com>
2026-06-11 10:38:23 +0700

    complete lesson 2.1 - 2.4

--- JP Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ JP Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -19,6 +19,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("GAME OVER!");
             Destroy(gameObject);
         }
     }
---
+++ JP Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    public void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[0].transform.rotation);
+    }
+
+}
