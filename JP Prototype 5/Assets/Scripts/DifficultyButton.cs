bc43fe33aad9704bcca79e47d2ce1a163a8d8d84
Finn191 <thanhphungab@gmail.com>
2026-06-15 18:20:17 +0700

    complete Mission: User Interface

---
+++ JP Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public int difficulty;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    private void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
