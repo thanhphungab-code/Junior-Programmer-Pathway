073d2c42be6662e22d3a57ec18d801c5aff9cfb0
Finn191 <thanhphungab@gmail.com>
2026-06-20 17:45:45 +0700

    Bonus: chore: update scene and project settings

--- JP Prototype 4/Assets/Scripts/PlayerController.cs
+++ JP Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,16 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed;
     public GameObject powerUpIndicator;
     private InputSystem_Actions controls;
-    private Rigidbody playerRb;
+    [SerializeField] private Rigidbody playerRb;
     private GameObject focalPoint;
-
-    private float powerUpStrength = 1000;
-    public bool hasPowerup = false;
-
+    // private float powerUpStrength = 1000;
+    // public bool hasPowerup = false;
+    // private IEquippable equippedAbility;
+    private Coroutine abilityTimeoutCoroutine;
+    private float nextReadyTime;
     private void Awake()
     {
         controls = new InputSystem_Actions();
-        playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
     private void OnEnable()
@@ -29,34 +29,59 @@ public class PlayerController : MonoBehaviour
         float forwardInput = moveInput.y;
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * playerSpeed * Time.deltaTime);
         powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        // if (equippedAbility != null && controls.Player.UseAbility.triggered)
+        // {
+        //     if (Time.time >= nextReadyTime)
+        //     {
+        //         // equippedAbility.OnUse(this);
+        //     }
+        // }
     }
 
-    private void OnTriggerEnter(Collider other)
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Powerup"))
+    //     {
+    //         powerUpIndicator.gameObject.SetActive(true);
+    //         hasPowerup = true;
+    //         Destroy(other.gameObject);
+    //         StartCoroutine(LoseAbilityAfterTime(equippedAbility.Duration));
+    //     }
+    // }
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+    //     {
+    //         Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+    //         Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+    //         enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Force);
+    //         Debug.Log("ABC");
+    //     }
+    // }
+
+    public IEnumerator LoseAbilityAfterTime(float duration)
     {
-        if (other.CompareTag("Powerup"))
-        {
-            powerUpIndicator.gameObject.SetActive(true);
-            hasPowerup = true;
-            Destroy(other.gameObject);
-            StartCoroutine(PowerupCountdownRoutine());
-        }
+        yield return new WaitForSeconds(duration);
+        // ResetAbility();
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
-        {
-            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
-            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Force);
-            Debug.Log("ABC");
-        }
-    }
+    // public void ResetAbility()
+    // {
+    //     if (equippedAbility != null)
+    //     {
+    //         equippedAbility.OnUnequip();
+    //         equippedAbility = null;
+    //     }
+    //     powerUpIndicator.gameObject.SetActive(false);
+    //     StopCoroutine(abilityTimeoutCoroutine);
+    // }
 
-    public IEnumerator PowerupCountdownRoutine()
-    {
-        yield return new WaitForSeconds(7);
-        hasPowerup = false;
-        powerUpIndicator.gameObject.SetActive(false);
-    }
+    // public void EquipAbility(IEquippable ability)
+    // {
+    //     ResetAbility();
+    //     equippedAbility = ability;
+    //     abilityTimeoutCoroutine = StartCoroutine(LoseAbilityAfterTime(equippedAbility.Duration));
+    // }
 }
